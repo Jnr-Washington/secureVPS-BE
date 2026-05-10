e89fb709e6bcf31470d5e403ff9ef7374c3c9599
Washington Junior <ochiengwash16@gmail.com>
2026-05-11 01:15:20 +0300

    fixed async function

--- db/session.py
+++ db/session.py
@@ -1,3 +1,4 @@
+from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from core.config import settings
 
@@ -5,6 +6,6 @@ engine = create_async_engine(settings.DATABASE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
